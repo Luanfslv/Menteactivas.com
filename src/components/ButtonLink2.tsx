0835000b947ac86117ff01694d62d0348a1f2b30
calopsitapreta <139503167+Luanfslv@users.noreply.github.com>
2023-11-13 18:37:33 -0300

    att

--- src/components/ButtonLink2.tsx
+++ src/components/ButtonLink2.tsx
@@ -6,7 +6,7 @@ interface ButtonLink2 {
 
 const ButtonLink2 = ({ children }: any) => {
   // Gere um ID exclusivo para cada checkbox
-  const uniqueId = `checkbox-${Math.random().toString(36).substr(2, 9)}`;
+  const uniqueId = `checkbox-${Math.random().toString(36)}`;
 
   return (
     <div className="text-center w-full flex md:w-auto px-4 lg:px-[11.11%] sm:mx-0 sm:max-w-[355px] md:max-w-[800px] py-[16px] items-center text-[14px] font-bold rounded-[5px] bg-white border md:border-[1.5px] border-[#6700a7] text-[#6700a7]  transform transition-transform duration-200">
@@ -19,3 +19,5 @@ const ButtonLink2 = ({ children }: any) => {
 }
 
 export default ButtonLink2;
+
+
